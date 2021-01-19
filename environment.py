31c831fc4c1c4865bd15eb55354a047c6a35382c
lipol <bartek.lipski@icloud.com>
2021-01-19 21:45:22 +0100

    Added custom environment

---
+++ environment.py
@@ -0,0 +1,101 @@
+import numpy as np
+import random
+import curses
+
+LEFT = 0
+DOWN = 1
+RIGHT = 2
+UP = 3
+
+
+class Environment:
+
+    def __init__(self, fileName):
+        self.lake = np.array(self.loadEnvironment(fileName))
+        # Environment dimensions
+        self.actions = 4  # left, down, right, up
+        self.states = self.lake.size
+        # Starting position and state
+        self.pos = np.argwhere(self.lake == 'S')[0]
+        self.state = self.pos[0] * len(self.lake[0]) + self.pos[1]
+        # Goal position
+        self.goal = np.argwhere(self.lake == 'G')[0]
+        # Current reward and status
+        self.reward = 0
+        self.done = False
+        # Terminal view initialization
+        self.view = None
+
+    def reset(self):
+        self.pos = np.argwhere(self.lake == 'S')[0]
+        self.state = self.pos[0] * len(self.lake[0]) + self.pos[1]
+        self.reward = 0
+        self.done = False
+        return self.state
+
+    def initCurses(self):
+        self.view = curses.initscr()
+        curses.curs_set(0)
+        curses.start_color()
+        curses.use_default_colors()
+        # Agent color definition
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
+        # Success color definition
+        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_GREEN)
+
+    def deInitCurses(self):
+        curses.endwin()
+
+    def loadEnvironment(self, fileName):
+        lake = []
+        with open(fileName, "r") as file:
+            for line in file:
+                lake.append(list(line.rstrip()))
+        return lake
+
+    def sampleAction(self):
+        action = random.randrange(0, self.actions)
+        return action
+
+    def step(self, action):
+        if action == LEFT:
+            self.pos[1] = max(self.pos[1] - 1, 0)
+        elif action == DOWN:
+            self.pos[0] = min(self.pos[0] + 1, len(self.lake)-1)
+        elif action == RIGHT:
+            self.pos[1] = min(self.pos[1] + 1,  len(self.lake[0])-1)
+        else:
+            self.pos[0] = max(self.pos[0] - 1, 0)
+
+        # Calculate state number from position cordinates
+        self.state = self.pos[0] * len(self.lake[0]) + self.pos[1]
+
+        if self.lake[self.pos[0], self.pos[1]] == 'G':
+            self.reward = 1
+            self.done = True
+        elif self.lake[self.pos[0], self.pos[1]] == 'H':
+            self.reward = 0
+            self.done = True
+        else:
+            self.reward = 0
+            self.done = False
+
+        return self.state, self.reward, self.done
+
+    def render(self):
+        temp = ''
+        for row in self.lake:
+            temp += ' '.join(row)
+            temp += "\n"
+
+        self.view.addstr(0, 0, temp)
+        self.view.addstr(self.pos[0], self.pos[1]*2,
+                         self.lake[self.pos[0], self.pos[1]], curses.color_pair(1))
+        if self.done == True:
+            if self.reward == 0:
+                self.view.addstr(len(self.lake) + 1, 0, "You failed!", curses.color_pair(1))
+            else:
+                self.view.addstr(len(self.lake) + 1, 0, "Success!", curses.color_pair(2))
+        else:
+            self.view.addstr(len(self.lake) + 1, 0, "           ", curses.color_pair(0))
+        self.view.refresh()
--- main.py
+++ main.py
@@ -1,8 +1,84 @@
-lake = []
+from environment import Environment
+import numpy as np
+import random
+import time
 
-with open("lake.dat", "r") as file:
-    for line in file:
-        lake.append(list(line.rstrip()))
+fileName = 'lake.dat'
+env = Environment(fileName)
+print(f'Loaded {fileName} environment.')
+q_table = np.zeros((env.states, env.actions))
 
-for l in lake:
-    print(l)
+
+# Algorithm parameters
+num_episodes = 10000
+max_steps = 100
+
+learning_rate = 0.5
+discount_rate = 0.98
+
+exploration_rate = 1.0
+max_exploration_rate = 1.0
+min_exploration_rate = 0.0
+exploration_decay_rate = 0.001
+
+rewards = []
+
+for episode in range(1, num_episodes + 1):
+    state = env.reset()
+    done = False
+    episode_reward = 0
+
+    for step in range(max_steps):
+        # Exploration to Exploitation trade-off
+        exploration_rate_treshold = random.uniform(0, 1)
+        if exploration_rate_treshold > exploration_rate and not np.all(q_table[state, :] == 0):
+            action = np.argmax(q_table[state, :])  # Exploitation -> action with maximum q-value
+        else:
+            action = env.sampleAction()  # Exploration -> random action
+
+        new_state, reward, done = env.step(action)  # Perform an action
+
+        # Update Q-table for Q(s,a)
+        q_table[state, action] = q_table[state, action] * (1 - learning_rate) \
+            + learning_rate * (reward + discount_rate * np.max(q_table[new_state, :]))
+
+        state = new_state
+        episode_reward += reward
+
+        if done == True:
+            break
+
+    # Exploration rate decay -> exploration rate decrase at a rate propotional to its current value
+    exploration_rate = min_exploration_rate + \
+        (max_exploration_rate - min_exploration_rate) * np.exp(-exploration_decay_rate*episode)
+
+    rewards.append(episode_reward)
+    if episode % 1000 == 0 and episode != 0:
+        print(f'Avg reward after {episode} episodes: {round(np.sum(rewards)/episode,2)}')
+    #print('Downloading File FooFile.txt [%d%%]\r' % i, end="")
+
+# print(q_table)
+# time.sleep(3)
+
+# Vizualization
+env.initCurses()
+num_episodes = 2
+for episode in range(1, num_episodes + 1):
+    state = env.reset()
+    done = False
+
+    for step in range(max_steps):
+        env.render()
+        time.sleep(0.2)
+        action = np.argmax(q_table[state, :])
+        new_state, reward, done = env.step(action)
+
+        if done:
+            env.render()
+            time.sleep(1)
+            break
+
+        state = new_state
+
+time.sleep(1)
+env.deInitCurses()
